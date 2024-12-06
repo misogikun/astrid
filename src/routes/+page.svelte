17e5311f8ac9281eefe49f160eeb007355ea80ce
Astrid <astrid@uziworld.com>
2024-12-06 22:53:50 +0100

    Bunch of updates: - Changed Page and Style, more images and stuff :3 - gitignore .svelte-kit - replaced image links for actual images - new font - new favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,19 +3,44 @@
 </script>
 
 <style>
-    :global(body) {
-        background-color: black;
-        color: white;
-    }
+  :global(body) {
+      background-color: black;
+      color: white;
+      font-family: 'hack', sans-serif;
+      font-size: small;
+  }
+
+  @font-face {
+      font-family: 'hack';
+      src: url('/src/fonts/HackNerdFont-Regular.ttf') format('truetype');
+  }
 </style>
 
-<div>
-  <img class=pfp src= "https://avatars.githubusercontent.com/u/148531582?v=4" alt="profile">
+<div class="container">
+  <div>
+  <img class="pfp" src= "https://avatars.githubusercontent.com/u/148531582?v=4" alt="profile">
   <div class="profile">
       <h1>Hi, i am Astrid :3</h1>
       <p>I am a 21 year old computer science student and software developer from germany. </p>
   </div>
+</div>
 
+<div>
+  <p>
+    I am losing it. I am losing it. I am losing it. I am losing it. I am losing it. I am losing it. I am losing it. I am losing it. I am losing it. I am losing it. I am losing it. I am losing it. I am losing it. I am losing it. I am losing it. I am losing it. I am losing it. I am losing it. I am losing it. 
+    I am losing it. I am losing it. I am losing it. I am losing it. I am losing it. I am losing it. 
+  </p>
+</div>
+
+<div>
+  <iframe title="elise" width="420" height="315"
+  src="https://www.youtube.com/embed/JCRQXwRfx-k?autoplay=1">
+  </iframe> 
+</div>
+
+<div>
+  <h2>Projects</h2>
+  <a href="https://github.com/KatsInCode/webscraper-gifs" target="_blank">Golang webscraper for gifs</a>
 </div>
 
 <div>
@@ -27,7 +52,24 @@
 
 <div>
   <h2>Cool stuff</h2>
-  <img src="https://web.archive.org/web/20090728044138/http://de.geocities.com/sbk1503/icon/vim.vialle.love.anim.gif" alt="vim">
-  <img src="https://99gifshop.neocities.org/img/88x31.png" alt="99gifshop">
-  <img src="https://web.archive.org/web/20091027000416/http://de.geocities.com/angel_luder/images/ANIgrnFishC.gif" alt="fish">
+  <img src="../src/images/88x31.png" alt="hiii">
+  <img src="../src/images/github.gif" alt="99gifshop">
+  <img src="../src/images/made-with-neovim.gif" alt="palestine">
+  <img src="../src/images/miku-approved.gif" alt="hiii">
+  <img src="../src/images/powered-by-nixos.png" alt="99gifshop">
+  <img src="../src/images/standwith.png" alt="palestine">
+  <img src="../src/images/transnow2.gif" alt="99gifshop">
+  <img src="../src/images/vim.vialle.love.anim.gif" alt="palestine">
+  <img src="../src/images/neofedi.gif" alt="hiii">
+  <img src="../src/images/catgirl-obsession.gif" alt="99gifshop">
+  <img src="../src/images/madeon_linux.gif" alt="palestine">
+  <img src="../src/images/debian.gif" alt="99gifshop">
+  <img src="../src/images/hl.gif" alt="palestine">
+  <img src="../src/images/wii.gif" alt="hiii">
+  <img src="../src/images/furret.gif" alt="99gifshop">
+  <img src="../src/images/poke.gif" alt="palestine">
+  <img src="../src/images/delete_twitter.gif" alt="99gifshop">
+  <img src="../src/images/bob.gif" alt="palestine">
+</div>
+
 </div>
